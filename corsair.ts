d6e829a36784bb9702292a161ca4c10f9bce91e6
Shyamendra Hazra <shyamendrahazra@gmail.com>
2026-06-19 02:30:01 +0530

    fix: removed manual client id and client secret passing

--- corsair.ts
+++ corsair.ts
@@ -5,17 +5,8 @@ import { googlecalendar } from "@corsair-dev/googlecalendar";
 
 export const db = new Pool({ connectionString: process.env.DATABASE_URL! });
 
-const googleOAuth = {
-  clientId: process.env.GOOGLE_CLIENT_ID!,
-  clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-  redirectUri: `${process.env.NEXT_PUBLIC_APP_URL || 'http://localhost:3000'}/api/corsair/oauth/callback`,
-};
-
 export const corsair = createCorsair({
-  plugins: [
-    gmail({ oauth: googleOAuth }),
-    googlecalendar({ oauth: googleOAuth }),
-  ],
+  plugins: [gmail(), googlecalendar()],
   database: db,
   kek: process.env.CORSAIR_KEK!,
   multiTenancy: true,

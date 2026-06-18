c0bb627e94fd66ee8837cfb5fc2ba09bd5836455
Shyamendra Hazra <shyamendrahazra@gmail.com>
2026-06-19 02:24:49 +0530

    fix: pass client id and secret

--- corsair.ts
+++ corsair.ts
@@ -5,8 +5,17 @@ import { googlecalendar } from "@corsair-dev/googlecalendar";
 
 export const db = new Pool({ connectionString: process.env.DATABASE_URL! });
 
+const googleOAuth = {
+  clientId: process.env.GOOGLE_CLIENT_ID!,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+  redirectUri: `${process.env.NEXT_PUBLIC_APP_URL || 'http://localhost:3000'}/api/corsair/oauth/callback`,
+};
+
 export const corsair = createCorsair({
-  plugins: [gmail(), googlecalendar()],
+  plugins: [
+    gmail({ oauth: googleOAuth }),
+    googlecalendar({ oauth: googleOAuth }),
+  ],
   database: db,
   kek: process.env.CORSAIR_KEK!,
   multiTenancy: true,
